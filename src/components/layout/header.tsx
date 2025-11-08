b6fa6f75ae420be030ede6342f4073fad6d1c619
Roderis Ortega <roderis0927@gmail.com>
2025-11-08 16:50:14 +0000

    Consultoría de Implementación de nuestras aplicaciones: Consideramos tod

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -230,20 +230,20 @@ const Content = ({
       id="overlay-content"
       initial={{
         opacity: 0,
+        y: 8,
       }}
       animate={{
         opacity: 1,
+        y: 0,
         width: selectedTab?.width,
       }}
       exit={{
         opacity: 0,
+        y: 8,
       }}
       transition={{
         duration: 0.25,
         ease: 'easeInOut',
-      }}
-      style={{
-        width: selectedTab ? selectedTab.width : 'auto',
       }}
       className="absolute left-0 top-[calc(100%_+_24px)] rounded-lg border border-slate-200 bg-gradient-to-b from-white via-white to-slate-50 p-4 shadow-lg"
     >
@@ -257,6 +257,7 @@ const Content = ({
               <motion.div
                 initial={{
                   opacity: 0,
+                  x: dir === 'l' ? 100 : dir === 'r' ? -100 : 0,
                 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.25, ease: 'easeInOut' }}
@@ -414,7 +415,7 @@ const ServicesContent = () => (
       icon={<Users className="w-4 h-4" />}
       title="Consultoría de Implementación"
       description="Expertos para asegurar su éxito."
-      href="#"
+      href="/consultoria-implementacion"
     />
     <MenuItem
       icon={<Code2 className="w-4 h-4" />}
