edb3f382729bef2eff504350b746985bc975ed09
Roderis Ortega <roderis0927@gmail.com>
2025-11-08 05:06:27 +0000

    Porque no hay en el header uno que diga Inicio y me lleve a la pagina pr

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,11 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const Logo = ({ className }: { className?: string }) => (
-  <a href="#" className={cn("flex items-center text-3xl font-bold", className)}>
+  <Link href="/" className={cn("flex items-center text-3xl font-bold", className)}>
     <span className="text-slate-800 tracking-tight">CPT</span>
     <span className="text-slate-500 tracking-tight">SOFT</span>
-  </a>
+  </Link>
 );
 
 export default Logo;
