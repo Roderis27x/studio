7d10200e877a4a6686b45bbe12e1c8b358c6d95c
Roderis27x <roderis0927@gmail.com>
2025-11-18 12:42:51 -0500

    refactor: simplificar el componente Logo eliminando imágenes y utilizando texto

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,32 +1,11 @@
-'use client';
-
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import Image from "next/image";
 
-const Logo = ({ className }: { className?: string }) => {
-  return (
-    <Link href="/" className={cn("flex items-center", className)}>
-      {/* Logo para modo claro */}
-      <Image
-        src="/img/logo_icono_negro.svg"
-        alt="CPT-SOFT Logo"
-        width={40}
-        height={40}
-        priority
-        className="dark:hidden block"
-      />
-      {/* Logo para modo oscuro */}
-      <Image
-        src="/img/logo_icono_blanco.svg"
-        alt="CPT-SOFT Logo"
-        width={40}
-        height={40}
-        priority
-        className="hidden dark:block"
-      />
-    </Link>
-  );
-};
+const Logo = ({ className }: { className?: string }) => (
+  <Link href="/" className={cn("flex items-center text-3xl font-bold", className)}>
+    <span className="text-slate-800 dark:text-white tracking-tight">CPT</span>
+    <span className="text-slate-500 dark:text-slate-400 tracking-tight">SOFT</span>
+  </Link>
+);
 
 export default Logo;
